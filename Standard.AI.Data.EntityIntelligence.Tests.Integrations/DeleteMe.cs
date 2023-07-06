2625cf4361db529790d71a154f8dc810dafd74b9
Hassan Habib <hassanhabib@live.com>
2023-07-05 21:32:56 -0700

    INFRA: Initialize Integration Tests
    Closes #5

---
+++ Standard.AI.Data.EntityIntelligence.Tests.Integrations/DeleteMe.cs
@@ -0,0 +1,15 @@
+// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xunit;
+
+namespace Standard.AI.Data.EntityIntelligence.Tests.Integrations
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            Assert.True(condition: true);
+    }
+}
