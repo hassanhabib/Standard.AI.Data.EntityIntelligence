35f5c2e36eadc794da651b069b29e50f94684795
Hassan Habib <hassanhabib@live.com>
2023-07-06 21:29:47 -0700

    BROKERS: Prompt AI Completion
    Closes #17

---
+++ Standard.AI.Data.EntityIntelligence/Brokers/AIs/AIBroker.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Standard.AI.Data.EntityIntelligence.Models.AIs;
+using Standard.AI.OpenAI.Clients.OpenAIs;
+using Standard.AI.OpenAI.Models.Configurations;
+using Standard.AI.OpenAI.Models.Services.Foundations.Completions;
+
+namespace Standard.AI.Data.EntityIntelligence.Brokers.AIs
+{
+    internal class AIBroker : IAIBroker
+    {
+        private readonly AIConfigurations configurations;
+        private readonly IOpenAIClient openAIClient;
+
+        public AIBroker(AIConfigurations configurations)
+        {
+            this.configurations = configurations;
+            this.openAIClient = InitializeOpenAIClient();
+        }
+
+        public async ValueTask<Completion> PromptCompletionAsync(Completion completion) =>
+            await this.openAIClient.Completions.PromptCompletionAsync(completion);
+
+        private IOpenAIClient InitializeOpenAIClient()
+        {
+            var openAIConfigurations = new OpenAIConfigurations();
+            openAIConfigurations.ApiKey = this.configurations.AIApiKey;
+
+            return new OpenAIClient(openAIConfigurations);
+        }
+    }
+}
