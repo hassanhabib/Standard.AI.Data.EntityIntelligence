dfd8b7be323ae6ca02867d18b6f4ae11bc386255
Kailu Hu <hukailu@gmail.com>
2023-07-06 22:04:58 -0700

    ShouldRetrieveSqlQueryAsync -> FAIL

---
+++ Standard.AI.Data.EntityIntelligence.Tests.Unit/Services/Foundations/AIs/AIServiceTests.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using KellermanSoftware.CompareNetObjects;
+using Moq;
+using Standard.AI.Data.EntityIntelligence.Brokers.AIs;
+using Standard.AI.Data.EntityIntelligence.Services.Foundations.AIs;
+using Standard.AI.OpenAI.Models.Services.Foundations.Completions;
+using Tynamix.ObjectFiller;
+
+namespace Standard.AI.Data.EntityIntelligence.Tests.Unit.Services.Foundations.AIs
+{
+    public partial class AIServiceTests
+    {
+        private readonly Mock<IAIBroker> aiBrokerMock;
+        private readonly IAIService aiService;
+        private readonly ICompareLogic compareLogic;
+
+        public AIServiceTests()
+        {
+            this.aiBrokerMock = new Mock<IAIBroker>();
+            this.compareLogic = new CompareLogic();
+
+            this.aiService = new AIService(
+                aiBroker: this.aiBrokerMock.Object);
+        }
+
+        private static string GetRandomString() => 
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 10, max: 20).GetValue();
+
+        private Expression<Func<Completion, bool>> SameCompletionAs(Completion expectedCompletion)
+        {
+            return actualCompletion => 
+                this.compareLogic.Compare(expectedCompletion, actualCompletion).AreEqual;
+        }
+    }
+}
---
+++ Standard.AI.Data.EntityIntelligence/Services/Foundations/AIs/AIService.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Standard.AI.Data.EntityIntelligence.Brokers.AIs;
+
+namespace Standard.AI.Data.EntityIntelligence.Services.Foundations.AIs
+{
+    internal class AIService : IAIService
+    {
+        private readonly IAIBroker aiBroker;
+
+        public AIService(IAIBroker aiBroker) =>
+            this.aiBroker = aiBroker;
+
+        public ValueTask<string> RetrieveSqlQueryAsync(string naturalQuery)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
