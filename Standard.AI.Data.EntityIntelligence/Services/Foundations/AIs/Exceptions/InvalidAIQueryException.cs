5a72a3700618f59a0c4bc0f65afa0d3388301843
Hassan Habib <hassanhabib@live.com>
2023-07-10 21:21:16 -0700

    ShouldThrowValidationExceptionOnRetrieveIfQueryIsInvalidAsync -> FAIL

---
+++ Standard.AI.Data.EntityIntelligence/Services/Foundations/AIs/Exceptions/InvalidAIQueryException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace Standard.AI.Data.EntityIntelligence.Services.Foundations.AIs.Exceptions
+{
+    internal class InvalidAIQueryException : Xeption
+    {
+        public InvalidAIQueryException()
+            : base(message: "Invalid AI Query error occurred, fix the errors and try again.")
+        { }
+    }
+}
