81078c19aba50ab9ec4c6d14cf543b7db0f2230c
Milan Toncic <milan.toncic@zuehlke.com>
2023-07-18 19:21:09 +0800

    Initializing DataBroker and retrieving model

---
+++ Standard.AI.Data.EntityIntelligence/Brokers/Data/DataBroker.cs
@@ -0,0 +1,32 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Standard.AI.Data.EntityIntelligence.Models.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using Dapper;
+
+namespace Standard.AI.Data.EntityIntelligence.Brokers.DataStorages
+{
+    public class DataBroker
+    {
+        private readonly DataStorageConfiguration configuration;
+
+        public DataBroker(DataStorageConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<IEnumerable<TableColumnMetadata>> SelectSqlTablesMetadataAsync()
+        {
+            const string query = "";
+
+            using var connection = new SqlConnection(configuration.ConnectionString);
+
+            return await connection.QueryAsync<TableColumnMetadata>(query);
+        }
+    }
+}
---
+++ Standard.AI.Data.EntityIntelligence/Brokers/Data/TableColumnMetadata.cs
@@ -0,0 +1,37 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+namespace Standard.AI.Data.EntityIntelligence.Brokers.DataStorages
+{
+    public class TableColumnMetadata
+    {
+        public string TableSchema { get; }
+
+        public string TableName { get; }
+
+        public string Name { get; }
+
+        public string Type { get; }
+
+        public int? MaxLength { get; }
+
+        public bool IsNullable { get; }
+
+        public bool IsPrimaryKey { get; }
+
+        public short? Ordinal { get; }
+
+        public bool IsForeignKey { get; }
+
+        public string ConstraintName { get; }
+
+        public string ReferencedConstraintName { get; }
+
+        public string ReferecedTableSchemaName { get; }
+
+        public string ReferencedTableName { get; }
+
+        public string ReferencedColumnName { get; }
+    }
+}
