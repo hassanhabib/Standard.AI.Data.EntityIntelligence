4465b32c73c3a726f6bed565ff2c3f06d7955b4e
Milan Toncic <milan.toncic@zuehlke.com>
2023-07-18 19:19:49 +0800

    Introducing configuration model

---
+++ Standard.AI.Data.EntityIntelligence/Models/Data/DataStorageConfiguration.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+namespace Standard.AI.Data.EntityIntelligence.Models.Data
+{
+    public class DataStorageConfiguration
+    {
+        public string ConnectionString { get; init; }
+    }
+}
