cb16b401353c5bb3b6ed2a2352c8d533e187f0ba
Kailu Hu <hukailu@gmail.com>
2023-07-18 21:53:44 -0700

    ShouldRetrieveSqlQueryAsync -> PASS

--- Standard.AI.Data.EntityIntelligence/Services/Processings/AIs/AIProcessingService.cs
+++ Standard.AI.Data.EntityIntelligence/Services/Processings/AIs/AIProcessingService.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Standard.AI.Data.EntityIntelligence.Models.Datas;
 using Standard.AI.Data.EntityIntelligence.Services.Foundations.AIs;
@@ -17,11 +18,25 @@ namespace Standard.AI.Data.EntityIntelligence.Services.Processings.AIs
         public AIProcessingService(IAIService aiService) =>
             this.aiService = aiService;
 
-        public ValueTask<string> RetrieveSqlQueryAsync(
+        public async ValueTask<string> RetrieveSqlQueryAsync(
             List<TableInformation> tables,
             string naturalQuery)
         {
-            throw new NotImplementedException();
+            string tableNameColumns = String.Join(" ", tables.Select(table =>
+            {
+                string tableName = table.Name;
+
+                string tableColumns =
+                    String.Join(" ", table.Columns.Select(column =>
+                        $"{column.Name} with type {column.Type}"));
+
+                return $"Table name: {tableName} has the following columns: {tableColumns}";
+            }));
+
+            string naturalQueryInput = $"Respond ONLY with code. Given a SQL db with the following tables: " +
+                $"{tableNameColumns} Translated the following request into SQL query: {naturalQuery}";
+
+            return await this.aiService.PromptQueryAsync(naturalQueryInput);
         }
     }
 }
