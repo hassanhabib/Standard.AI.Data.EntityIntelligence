9bab892f9cc3675edaba0fd4a81a40c387ef79d8
Milan Toncic <milantoncic@outlook.com>
2023-07-22 17:41:45 +0800

    To retrieve db model

---
+++ Standard.AI.Data.EntityIntelligence/Models/Datas/TableColumnMetadata.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+namespace Standard.AI.Data.EntityIntelligence.Models.Datas
+{
+    public class TableColumnMetadata
+    {
+        public string TableSchema { get; }
+        public string TableName { get; }
+        public string Name { get; }
+        public string Type { get; }
+    }
+}
