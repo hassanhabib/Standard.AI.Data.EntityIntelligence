4f9c4923d5d2a6990a4862039889dbab04ffc058
Milan Toncic <milantoncic@outlook.com>
2023-07-23 15:09:42 +0800

    ShouldRetrieveTablesDetailsAsync -> FAIL

---
+++ Standard.AI.Data.EntityIntelligence.Tests.Unit/Services/Foundations/Datas/DataServiceTests.Logics.Retrieve.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standard.AI.Data.EntityIntelligence.Models.Datas.Brokers;
+using Standard.AI.Data.EntityIntelligence.Models.Datas.Services;
+using Xunit;
+
+namespace Standard.AI.Data.EntityIntelligence.Tests.Unit.Services.Foundations.Datas
+{
+    public partial class DataServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveTablesDetailsAsync()
+        {
+            var randomQuery = GetRandomString();
+            var randomNumber = GetRandomNumber();
+            var randomTablesMetadataProperties = GenerateRandomTablesMetadata();
+
+            var toRetrieveTablesColumnsMetadata =
+                randomTablesMetadataProperties.Keys.SelectMany(schema__tableName =>
+                {
+                    var columnsMetadata = randomTablesMetadataProperties[schema__tableName];
+                    var tablesColumnsMetadata = new List<TableColumnMetadata>();
+
+                    foreach (var columnKey in columnsMetadata.Keys)
+                    {
+                        var columnMetadata = columnsMetadata[columnKey];
+
+                        tablesColumnsMetadata.Add(
+                            new TableColumnMetadata
+                            {
+                                TableSchema = schema__tableName.Split("__")[0],
+                                TableName = schema__tableName.Split("__")[1],
+                                Name = columnKey,
+                                Type = columnsMetadata[columnKey],
+                            });
+                    }
+
+                    return tablesColumnsMetadata;
+                });
+
+            var expectedTablesDetails = 
+                randomTablesMetadataProperties.Keys.Select(schema__tableName =>
+                {
+                    var columnsMetadata = randomTablesMetadataProperties[schema__tableName];
+
+                    return new TableMetadata
+                    {
+                        Schema = schema__tableName.Split("__")[0],
+                        Name = schema__tableName.Split("__")[1],
+                        ColumnsMetadata = columnsMetadata.Keys.Select(key => new ColumnMetadata
+                        {
+                            Name = key,
+                            DataType = columnsMetadata[key],
+                        }).ToList()
+                    };
+                }).ToList();
+
+            dataBrokerMock.Setup(broker => broker.ExecuteQueryAsync<TableColumnMetadata>(randomQuery))
+                .ReturnsAsync(toRetrieveTablesColumnsMetadata);
+
+            // when
+            var retrievedTablesDetails =
+                await dataService.RetrieveTablesDetailsAsync();
+
+            retrievedTablesDetails.Should().BeEquivalentTo(expectedTablesDetails);
+
+            dataBrokerMock.Verify(broker =>
+                broker.ExecuteQueryAsync<TableColumnMetadata>(randomQuery),
+                    Times.Once());
+
+            dataBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Standard.AI.Data.EntityIntelligence.Tests.Unit/Services/Foundations/Datas/DataServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Moq;
+using Standard.AI.Data.EntityIntelligence.Brokers.Datas;
+using Standard.AI.Data.EntityIntelligence.Services.Foundations.Datas;
+using Tynamix.ObjectFiller;
+
+namespace Standard.AI.Data.EntityIntelligence.Tests.Unit.Services.Foundations.Datas
+{
+    public partial class DataServiceTests
+    {
+        private readonly Mock<IDataBroker> dataBrokerMock;
+        private readonly IDataService dataService;
+
+        public DataServiceTests()
+        {
+            this.dataBrokerMock = new Mock<IDataBroker>();
+
+            this.dataService = new DataService(
+                dataBroker: this.dataBrokerMock.Object);
+        }
+
+        private static string GetRandomString() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static int GetRandomNumber(int min = 5, int max = 10) =>
+            new IntRange(min, max).GetValue();
+
+        private static Dictionary<string, Dictionary<string, string>> GenerateRandomTablesMetadata()
+        {
+            int tablesCount = GetRandomNumber();
+            var tablesDictionary = new Dictionary<string, Dictionary<string, string>>();
+
+            for (int i = 0; i < tablesCount; i++)
+            {
+                (string schema__tableName, Dictionary<string, string> tableData) =
+                    GenerateRandomTable();
+
+                tablesDictionary.Add(schema__tableName, tableData);
+            }
+
+            return tablesDictionary;
+        }
+
+        private static Tuple<string, Dictionary<string, string>> GenerateRandomTable()
+        {
+            string randomTableName = GetRandomString();
+            string randomSchemaName = GetRandomString();
+
+            int randomColumnCount = GetRandomNumber();
+            var columnsDictionary = new Dictionary<string, string>();
+
+            for (int i = 0; i <= randomColumnCount; i++)
+            {
+                columnsDictionary.Add(
+                    key: GetRandomString(),
+                    value: GetRandomString());
+            }
+
+            return new($"{randomSchemaName}__{randomTableName}", columnsDictionary);
+        }
+    }
+}
