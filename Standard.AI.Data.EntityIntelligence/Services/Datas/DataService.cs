780123b8f2a5090242781790e1a7fbf1e7beea45
Milan Toncic <milantoncic@outlook.com>
2023-07-24 21:29:17 +0800

    ShouldRetrieveTablesDetailsAsync -> PASS

--- Standard.AI.Data.EntityIntelligence/Services/Datas/DataService.cs
+++ Standard.AI.Data.EntityIntelligence/Services/Datas/DataService.cs
@@ -4,8 +4,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Standard.AI.Data.EntityIntelligence.Brokers.Datas;
+using Standard.AI.Data.EntityIntelligence.Models.Datas.Brokers;
 using Standard.AI.Data.EntityIntelligence.Models.Datas.Services;
 
 namespace Standard.AI.Data.EntityIntelligence.Services.Foundations.Datas
@@ -19,7 +21,47 @@ namespace Standard.AI.Data.EntityIntelligence.Services.Foundations.Datas
 
         public async ValueTask<List<TableMetadata>> RetrieveTablesDetailsAsync()
         {
-            throw new NotImplementedException();
+            var retrievedTablesColumnsMetadata =
+              await this.dataBroker.ExecuteQueryAsync<TableColumnMetadata>(SelectAllTablesMetadataQuery);
+
+            return ToTablesMetadata(retrievedTablesColumnsMetadata);
         }
+
+        private static List<TableMetadata> ToTablesMetadata(
+            IEnumerable<TableColumnMetadata> retrievedTablesColumnsMetadata)
+        {
+            var groupedColumns =
+                retrievedTablesColumnsMetadata
+                    .GroupBy(tableColumnMetadata =>
+                        (tableColumnMetadata.TableSchema, tableColumnMetadata.TableName));
+
+            return groupedColumns.Select(ToTableMetadata).ToList();
+
+            static TableMetadata ToTableMetadata(
+                IGrouping<(string TableSchema, string Name),
+                TableColumnMetadata> tableColumnsMetadata) =>
+                new()
+                {
+                    Schema = tableColumnsMetadata.Key.TableSchema,
+                    Name = tableColumnsMetadata.Key.Name,
+                    ColumnsMetadata =
+                        tableColumnsMetadata.Select(columnsMetadata =>
+                            new ColumnMetadata
+                            {
+                                Name = columnsMetadata.Name,
+                                DataType = columnsMetadata.DataType,
+                            }).ToList(),
+                };
+        }
+
+        private static readonly string SelectAllTablesMetadataQuery =
+            String.Join(
+                Environment.NewLine,
+                "SELECT",
+                $"c.TABLE_SCHEMA AS [{nameof(TableColumnMetadata.TableSchema)}],",
+                $"c.TABLE_NAME AS [{nameof(TableColumnMetadata.TableName)}],",
+                $"c.COLUMN_NAME AS [{nameof(TableColumnMetadata.Name)}],",
+                $"c.DATA_TYPE AS [{nameof(TableColumnMetadata.DataType)}]",
+                "FROM INFORMATION_SCHEMA.COLUMNS c");
     }
 }
