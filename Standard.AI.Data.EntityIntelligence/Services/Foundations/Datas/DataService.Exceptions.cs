effc1f2c1599e55ac1734e4ce2ed83b58496fd9e
Milan Toncic <milantoncic@outlook.com>
2023-07-24 21:47:17 +0800

    ShouldThrowDependencyExceptionOnSqlException -> PASS

--- Standard.AI.Data.EntityIntelligence/Services/Foundations/Datas/DataService.Exceptions.cs
+++ Standard.AI.Data.EntityIntelligence/Services/Foundations/Datas/DataService.Exceptions.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Standard.AI.Data.EntityIntelligence.Models.Datas.Services;
 using Standard.AI.Data.EntityIntelligence.Models.Foundations.Datas.Exceptions;
@@ -33,6 +34,13 @@ namespace Standard.AI.Data.EntityIntelligence.Services.Foundations.Datas
                     new FailedDataStorageDependencyValidationException(argumentException);
 
                 throw new DataStorageDependencyValidationException(failedDependencyValidationException);
+            }
+            catch (SqlException sqlException)
+            {
+                var failedDataStorageDependencyException =
+                    new FailedDataStorageDependencyValidationException(sqlException);
+
+                throw new DataStorageDependencyException(failedDataStorageDependencyException);
             }
         }
     }
