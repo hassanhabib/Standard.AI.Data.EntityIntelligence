0a2b6f9780e45ebf84d9a8600fd686f05fc5d838
Milan Toncic <milantoncic@outlook.com>
2023-07-30 11:30:07 +0800

    ShouldThrowValidationExceptionOnRunQueryIfQueryIsInvalidAsync -> FAIL

---
+++ Standard.AI.Data.EntityIntelligence.Tests.Unit/Services/Foundations/Datas/DataServiceTests.Validations.RunQuery.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using Standard.AI.Data.EntityIntelligence.Models.Datas.Services;
+using Standard.AI.Data.EntityIntelligence.Models.Foundations.Datas.Exceptions;
+using Xunit;
+
+namespace Standard.AI.Data.EntityIntelligence.Tests.Unit.Services.Foundations.Datas
+{
+    public partial class DataServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public async Task ShouldThrowValidationExceptionOnRunQueryIfQueryIsInvalidAsync(
+            string invalidQuery)
+        {
+            // given
+            var invalidDataQueryException =
+                new InvalidDataQueryException();
+
+            var expectedDataValidationException =
+                new DataValidationException(invalidDataQueryException);
+
+            ValueTask<IEnumerable<ResultRow>> runQueryTask = 
+                this.dataService.RunQueryAsync(invalidQuery);
+
+            DataValidationException actualDataValidationException =
+                await Assert.ThrowsAsync<DataValidationException>(
+                    runQueryTask.AsTask);
+
+            this.dataBrokerMock.Verify(broker =>
+                broker.ExecuteQueryAsync<It.IsAnyType>(It.IsAny<string>()),
+                    Times.Never);
+
+            this.dataBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Standard.AI.Data.EntityIntelligence/Models/Foundations/Datas/Exceptions/DataValidationException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace Standard.AI.Data.EntityIntelligence.Models.Foundations.Datas.Exceptions
+{
+    internal class DataValidationException : Xeption
+    {
+        public DataValidationException(Xeption innerException)
+            : base(message: "Data validation error occurred, fix the errors and try again.")
+        { }
+    }
+}
