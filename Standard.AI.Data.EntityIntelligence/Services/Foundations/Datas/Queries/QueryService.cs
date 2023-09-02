1193d0e83200038ac19f5cb5d45e1de84454bd4f
Milan Toncic <milantoncic@outlook.com>
2023-09-02 16:46:06 +0800

    ShouldRunQueryAsync -> PASS

--- Standard.AI.Data.EntityIntelligence/Services/Foundations/Datas/Queries/QueryService.cs
+++ Standard.AI.Data.EntityIntelligence/Services/Foundations/Datas/Queries/QueryService.cs
@@ -3,6 +3,7 @@
 // ----------------------------------------------------------------------------------
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Standard.AI.Data.EntityIntelligence.Brokers.Datas;
 using Standard.AI.Data.EntityIntelligence.Models.Datas.Services.Queries;
@@ -16,9 +17,24 @@ namespace Standard.AI.Data.EntityIntelligence.Services.Foundations.Datas.Queries
         public QueryService(IDataBroker dataBroker) =>
             this.dataBroker = dataBroker;
 
-        public ValueTask<IEnumerable<ResultRow>> RunQueryAsync(string query)
+        public async ValueTask<IEnumerable<ResultRow>> RunQueryAsync(string query)
         {
-            throw new System.NotImplementedException();
+            IEnumerable<IDictionary<string, object>> retrievedRows =
+                await this.dataBroker.ExecuteQueryAsync<IDictionary<string, object>>(query);
+
+            return ToResultRows(retrievedRows);
         }
+
+        private static IEnumerable<ResultRow> ToResultRows(
+            IEnumerable<IDictionary<string, object>> retrievedRows) =>
+                retrievedRows.Select(row =>
+                    new ResultRow
+                    {
+                        Columns = row.Select(column => new ColumnData
+                        {
+                            Name = column.Key,
+                            Value = column.Value,
+                        })
+                    });
     }
 }
