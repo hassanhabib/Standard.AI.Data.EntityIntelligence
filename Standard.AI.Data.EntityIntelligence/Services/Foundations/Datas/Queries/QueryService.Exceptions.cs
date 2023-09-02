83197e7f46fd43ba53294b826710bf172a50be0f
Milan Toncic <milantoncic@outlook.com>
2023-09-02 18:12:52 +0800

    ShouldThrowSqlDependencyExceptionOnRunQueryIfSqlDependencyExceptionOccursAsync -> PASS

--- Standard.AI.Data.EntityIntelligence/Services/Foundations/Datas/Queries/QueryService.Exceptions.cs
+++ Standard.AI.Data.EntityIntelligence/Services/Foundations/Datas/Queries/QueryService.Exceptions.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Standard.AI.Data.EntityIntelligence.Models.Datas.Services.Queries;
 using Standard.AI.Data.EntityIntelligence.Models.Foundations.Datas.Exceptions;
@@ -37,6 +38,13 @@ namespace Standard.AI.Data.EntityIntelligence.Services.Foundations.Datas.Queries
             {
                 var invalidQueryException =
                     new InvalidQueryException(argumentException);
+
+                throw new QueryServiceDependencyValidationException(invalidQueryException);
+            }
+            catch (SqlException sqlException)
+            {
+                var invalidQueryException =
+                    new InvalidQueryException(sqlException);
 
                 throw new QueryServiceDependencyValidationException(invalidQueryException);
             }
