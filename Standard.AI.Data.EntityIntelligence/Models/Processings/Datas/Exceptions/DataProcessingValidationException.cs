5ffa5f3ee7064d92b4bb5275fa1b9ac602f2dd31
Kailu Hu <hukailu@gmail.com>
2023-10-02 21:39:27 -0700

    ShouldThrowValidationExceptionOnRetrieveIfInputQueryIsInvalidAsync -> FAIL

---
+++ Standard.AI.Data.EntityIntelligence/Models/Processings/Datas/Exceptions/DataProcessingValidationException.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace Standard.AI.Data.EntityIntelligence.Models.Processings.Datas.Exceptions
+{
+    internal class DataProcessingValidationException : Xeption
+    {
+        public DataProcessingValidationException(Xeption innerException)
+         : base(message: "Data validation error occurred, fix errors and try again.",
+               innerException)
+        { }
+
+        public DataProcessingValidationException(string message, Xeption innerException)
+            : base(message: message, innerException)
+        { }
+    }
+}
---
+++ Standard.AI.Data.EntityIntelligence/Models/Processings/Datas/Exceptions/InvalidQueryProcessingValidationException.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace Standard.AI.Data.EntityIntelligence.Models.Processings.Datas.Exceptions
+{
+    internal class InvalidQueryProcessingException : Xeption
+    {
+        public InvalidQueryProcessingException(Exception innerException)
+           : base(message: "Invalid Query errors occurred, fix the errors and try again.", innerException)
+        { }
+
+        public InvalidQueryProcessingException(string message)
+           : base(message) { }
+    }
+}
