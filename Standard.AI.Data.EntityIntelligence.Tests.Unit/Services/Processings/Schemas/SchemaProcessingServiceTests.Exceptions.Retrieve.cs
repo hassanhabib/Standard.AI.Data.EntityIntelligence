d1a001350dde3e49d730079c310154f50cf4c249
Kailu Hu <hukailu@gmail.com>
2023-10-18 21:31:21 -0700

    ShouldThrowDependencyValidationExceptionOnRetrieveIfSchemaDependencyValidationErrorAsync -> FAIL

---
+++ Standard.AI.Data.EntityIntelligence.Tests.Unit/Services/Processings/Schemas/SchemaProcessingServiceTests.Exceptions.Retrieve.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standard.AI.Data.EntityIntelligence.Models.Foundations.Schemas;
+using Standard.AI.Data.EntityIntelligence.Models.Foundations.Schemas.Exceptions;
+using Standard.AI.Data.EntityIntelligence.Models.Processings.Schemas.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace Standard.AI.Data.EntityIntelligence.Tests.Unit.Services.Processings.Schemas
+{
+    public partial class SchemaProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnRetrieveIfSchemaDependencyValidationErrorAsync()
+        {
+            // given
+            var someInnerException = new Xeption();
+
+            var schemaDependencyValidationException =
+                new SchemaDependencyValidationException(someInnerException);
+
+            var expectedSchemaProcessingDependencyValidationException =
+                new SchemaProcessingDependencyValidationException(
+                   message: "Data dependency validation error occurred, fix errors and try again.",
+                   innerException: someInnerException);
+
+            this.schemaServiceMock.Setup(service =>
+                service.RetrieveSchemaAsync())
+                    .ThrowsAsync(schemaDependencyValidationException);
+
+            // when
+            ValueTask<Schema> retrieveSchemaTask =
+                this.schemaProcessingService.RetrieveSchemaAsync();
+
+            SchemaProcessingDependencyValidationException actualSchemaProcessingDependencyValidationException =
+                await Assert.ThrowsAsync<SchemaProcessingDependencyValidationException>(
+                    retrieveSchemaTask.AsTask);
+
+            // then
+            actualSchemaProcessingDependencyValidationException.Should()
+                .BeEquivalentTo(expectedSchemaProcessingDependencyValidationException);
+
+            this.schemaServiceMock.Verify(service =>
+                service.RetrieveSchemaAsync(),
+                    Times.Once());
+
+            this.schemaServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Standard.AI.Data.EntityIntelligence/Models/Processings/Schemas/Exceptions/SchemaProcessingDependencyValidationException.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace Standard.AI.Data.EntityIntelligence.Models.Processings.Schemas.Exceptions
+{
+    internal class SchemaProcessingDependencyValidationException : Xeption
+    {
+        public SchemaProcessingDependencyValidationException(Xeption innerException)
+            : base(message: "Schema dependency validation error occurred, fix errors and try again.",
+                  innerException)
+        { }
+
+        public SchemaProcessingDependencyValidationException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}
